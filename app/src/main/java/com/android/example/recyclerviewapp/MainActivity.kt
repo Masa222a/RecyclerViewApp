90e7120797e3747783ca183ba8b5e20d57e2de61
Masa222a <guitarstring0716@gmail.com>
2022-05-07 22:12:21 +0900

    Initial commit

---
+++ app/src/main/java/com/android/example/recyclerviewapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.android.example.recyclerviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.example.recyclerviewapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val items = Array(20) { i -> "item-$i" }
+
+        val viewManager = LinearLayoutManager(this)
+        val viewAdapter = MyAdapter(items)
+
+        binding.recyclerView.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+    }
+}
+
+class MyAdapter(private val items: Array<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val textView = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false) as TextView
+        return ViewHolder(textView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+}
